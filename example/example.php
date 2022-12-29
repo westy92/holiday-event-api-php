8d7e9ad13b95cf48b7bdb2fc628b7db019736ba1
Seth Westphal <westy92@gmail.com>
2022-12-29 14:36:50 -0600

    Add example.

--- example/example.php
+++ example/example.php
@@ -1,31 +1,7 @@
-# The Official Holiday and Event API for PHP
+<?php
 
-<!-- TODO packagist -->
-<!-- TODO PHP version -->
-[![Build Status](https://github.com/westy92/holiday-event-api-php/actions/workflows/ci.yml/badge.svg)](https://github.com/westy92/holiday-event-api-php/actions)
-[![Code Coverage](https://codecov.io/gh/westy92/holiday-event-api-php/branch/main/graph/badge.svg)](https://codecov.io/gh/westy92/holiday-event-api-php)
-[![Type Coverage](https://shepherd.dev/github/westy92/holiday-event-api-php/coverage.svg)](https://shepherd.dev/github/westy92/holiday-event-api-php)
-[![Funding Status](https://img.shields.io/github/sponsors/westy92)](https://github.com/sponsors/westy92)
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
-Industry-leading Holiday and Event API for PHP. Over 5,000 holidays and thousands of descriptions. Trusted by the World’s leading companies. Built by developers for developers since 2011.
-
-## Supported PHP Versions
-
-Latest version of the the Holiday and Event API supports all currently-supported PHP [releases](https://endoflife.date/php).
-
-## Authentication
-
-Access to the Holiday and Event API requires an API Key. You can get for one for FREE [here](https://apilayer.com/marketplace/checkiday-api#pricing), no credit card required! Note that free plans are limited. To access more data and have more requests, a paid plan is required.
-
-## Installation
-
-```
-TODO
-```
-
-## Example
-
-```php
 try {
     // Get a FREE API key from https://apilayer.com/marketplace/checkiday-api#pricing
     $client = new Westy92\HolidayEventApi\Client('<your API key>');
@@ -66,4 +42,3 @@ try {
 } catch (\Exception $e) {
     echo $e;
 }
-```
