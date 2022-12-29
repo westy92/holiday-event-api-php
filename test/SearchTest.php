ac787723c1ec3af7cba0ec688f1b9c9cdfc36e39
Seth Westphal <westy92@gmail.com>
2022-12-29 11:23:22 -0600

    Add remaining search tests.

--- test/SearchTest.php
+++ test/SearchTest.php
@@ -75,4 +75,60 @@ final class SearchTest extends TestCase
         $expected->url = 'https://www.checkiday.com/61363236f06e4eb8e4e14e5925c2503d/sneak-some-zucchini-onto-your-neighbors-porch-day';
         $this->assertEquals($expected, $result->events[0]);
     }
+
+    public function testSearchQueryTooShort()
+    {
+        $this->mock->append(
+            function (\GuzzleHttp\Psr7\Request $request) {
+                $query = [];
+                parse_str($request->getUri()->getQuery(), $query);
+                $this->assertEquals([
+                    'adult' => 'false',
+                    'query' => 'a',
+                ], $query);
+                
+                return new Response(
+                    400,
+                    [],
+                    json_encode(['error' => 'Please enter a longer search term.']),
+                );
+            },
+        );
+        $client = new TestClient('abc123', $this->mock);
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Please enter a longer search term.');
+        $client->search('a');
+    }
+
+    public function testSearchTooManyResults()
+    {
+        $this->mock->append(
+            function (\GuzzleHttp\Psr7\Request $request) {
+                $query = [];
+                parse_str($request->getUri()->getQuery(), $query);
+                $this->assertEquals([
+                    'adult' => 'false',
+                    'query' => 'day',
+                ], $query);
+                
+                return new Response(
+                    400,
+                    [],
+                    json_encode(['error' => 'Too many results returned. Please refine your query.']),
+                );
+            },
+        );
+        $client = new TestClient('abc123', $this->mock);
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Too many results returned. Please refine your query.');
+        $client->search('day');
+    }
+
+    public function testSearchMissingParameters()
+    {
+        $client = new TestClient('abc123', $this->mock);
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Search query is required.');
+        $client->search('');
+    }
 }
